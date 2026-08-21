a1a828725aaae9c32ffddb029c3771fdf749e31c
adamoomar110-sys <adamoomar110@gmail.com>
2026-08-21 11:27:21 -0300

    chore: separar spinaz_garage_flota a repo independiente (cheq-flota-de-autos)

--- spinaz_garage_flota/web/src/app/postular/page.tsx
+++
@@ -1,5 +0,0 @@
-import PostulationForm from '@/components/postular/PostulationForm';
-
-export default function PostularPage() {
-  return <PostulationForm />;
-}
--- spinaz_garage_flota/web/src/components/SplashScreen.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-export default function SplashScreen() {
-  return null;
-}
-
